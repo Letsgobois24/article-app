3d7182721b5c4b3d455651c9cb27e956d879bd34
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-01-20 09:48:59 +0700

    fix: reactive monthly graphic

--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @livewireStyles
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
     <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap"
@@ -27,9 +28,8 @@
 
     <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     @livewireScripts
-    {{-- @livewireChartsScripts --}}
+    @livewireChartsScripts
     <script src="{{ asset('js/slugify.js') }}"></script>
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
     {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
 </body>
 
--- resources/views/livewire/components/dashboard/home/monthly-graphic.blade.php
+++ resources/views/livewire/components/dashboard/home/monthly-graphic.blade.php
@@ -6,8 +6,7 @@
             <option value="">No post yet</option>
         @endforelse
     </select>
-    <div wire:ignore class="bg-white p-4 rounded-lg h-96">
-        <livewire:livewire-column-chart wire:key='monthly-chart-{{ $selectedYear }}' key="{{ $chart->reactiveKey() }}"
-            :column-chart-model="$chart" />
+    <div class="bg-white p-4 rounded-lg h-96">
+        <livewire:livewire-column-chart key="{{ $chart->reactiveKey() }}" :column-chart-model="$chart" />
     </div>
 </section>
