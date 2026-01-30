630eb6b58e1199fc13f06c3a542eb9df6a0ce510
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-01-30 22:00:52 +0700

    refactor: modal

--- resources/views/components/dashboard/categories/modal.blade.php
+++ resources/views/components/dashboard/categories/modal.blade.php
@@ -1,38 +1,13 @@
-@props(['isEdit'])
-
-<x-modal.layout title="{{ $isEdit ? 'Edit' : 'Create' }} Category">
-    <x-slot:button>
-        {{ $slot }}
-    </x-slot:button>
-    {{-- Loading --}}
-    <div class="pt-4 md:pt-6 w-full flex flex-col space-y-2" wire:loading
-        wire:target="{{ $isEdit ? 'showEditModal' : 'resetForm' }}">
-        <x-form.skeleton-input label="Category Name" />
-        <x-form.skeleton-input label="Slug" />
-        {{-- Color Picker --}}
-        <div class="mb-4.5">
-            <label class="block mb-2 text-sm font-medium text-gray-900">Color</label>
-            <div class="animate-pulse flex items-center">
-                <div class="size-10 bg-gray-200 rounded border shadow-sm mr-2"></div>
-                <div class="h-5 bg-gray-300 rounded w-1/5"></div>
-            </div>
-        </div>
-        <button disabled
-            class="flex w-full justify-center items-center rounded-md bg-indigo-600 px-3 h-9 text-sm/6 font-semibold text-white shadow-xs">
-            <div class="w-4 h-4 border-2 mx-auto border-gray-300 border-t-blue-600 rounded-full animate-spin">
-            </div>
-        </button>
-    </div>
-
-    {{-- After Loading --}}
-    <form wire:submit.prevent='save' class="pt-4 md:pt-6 flex flex-col space-y-2" wire:loading.remove
-        wire:target="{{ $isEdit ? 'showEditModal' : 'resetForm' }}" x-data="{ name: @entangle('name'), slug: @entangle('slug') }">
+<x-modal.layout>
+    <form wire:submit.prevent='save' class="pt-4 md:pt-6 flex flex-col space-y-2">
         {{-- Name --}}
         <x-form.input name="name" label="Category Name" />
         {{-- Slug --}}
         <x-form.slug-input from="name" />
         {{-- Color Picker --}}
         <x-form.color-picker name="color" label="Color" />
-        <x-ui.submit-button target="save">{{ $isEdit ? 'Edit' : 'Create' }}</x-ui.submit-button>
+        <x-ui.submit-button target="save">
+            <span x-text="id ? 'Edit' : 'Create'"></span>
+        </x-ui.submit-button>
     </form>
 </x-modal.layout>
--- resources/views/livewire/pages/dashboard/category.blade.php
+++ resources/views/livewire/pages/dashboard/category.blade.php
@@ -78,35 +78,6 @@
 
     {{-- Modal --}}
     <div x-show="isOpenModal" x-cloak>
-        <!-- Main modal -->
-        <div
-            class="bg-black/50 overflow-hidden fixed flex top-0 right-0 left-0 z-20 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-            <div class="relative p-4 w-full max-w-md max-h-full">
-                <!-- Modal content -->
-                <div class="relative bg-neutral-primary-soft border border-default rounded-base shadow-sm p-4 md:p-6">
-                    <!-- Modal header -->
-                    <div class="flex items-center justify-between border-b border-default pb-4 md:pb-5">
-                        <h3 x-text="id ? 'Edit Category' : 'Add New Category'" class="text-lg font-medium text-heading">
-                        </h3>
-                        <button x-on:click="isOpenModal=false" type="button"
-                            class="cursor-pointer text-body bg-transparent hover:bg-neutral-tertiary hover:text-heading rounded-base text-sm w-9 h-9 ms-auto inline-flex justify-center items-center">
-                            <x-icons.cross size="20" />
-                        </button>
-                    </div>
-                    <!-- Modal body -->
-                    <form wire:submit.prevent='save' class="pt-4 md:pt-6 flex flex-col space-y-2">
-                        {{-- Name --}}
-                        <x-form.input name="name" label="Category Name" />
-                        {{-- Slug --}}
-                        <x-form.slug-input from="name" />
-                        {{-- Color Picker --}}
-                        <x-form.color-picker name="color" label="Color" />
-                        <x-ui.submit-button target="save">
-                            <span x-text="id ? 'Edit' : 'Create'"></span>
-                        </x-ui.submit-button>
-                    </form>
-                </div>
-            </div>
-        </div>
+        <x-dashboard.categories.modal />
     </div>
 </div>
