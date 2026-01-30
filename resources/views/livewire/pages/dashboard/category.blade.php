ac3cb83ed0458994ef3a33d3ee8b4bef502fceb3
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-01-30 17:40:47 +0700

    feat: add esc key for closing modal

--- resources/views/livewire/pages/dashboard/category.blade.php
+++ resources/views/livewire/pages/dashboard/category.blade.php
@@ -6,7 +6,7 @@
     color: @entangle('color'),
     lastColor: @entangle('lastColor'),
     lastSlug: @entangle('lastSlug')
-}">
+}" @keyup.escape="isOpenModal=false">
     <x-title>Post Categories</x-title>
     <div class="flex justify-between items-center mb-8">
         {{-- Search --}}
@@ -14,6 +14,7 @@
         {{-- Add Category --}}
         <button
             class="cursor-pointer flex gap-x-1.5 text-sm items-center text-white bg-brand box-border border border-transparent hover:bg-brand-strong focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base px-3.5 py-2.5 focus:outline-none"
+            wire:click='resetErrorInput'
             x-on:click="
                 isOpenModal=true;
                 id=null;
