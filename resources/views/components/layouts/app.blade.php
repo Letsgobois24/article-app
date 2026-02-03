b49801ebbaa46324b7c815a78e743ad5771c0ef7
Letsgobois24 <rayhanfariz@students.undip.ac.id>
2026-02-03 10:15:30 +0700

    refactor: layout

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,27 +1,13 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<x-layouts.layout>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap"
-        rel="stylesheet" />
-    <title>{{ $title }}</title>
-</head>
-
-<body class="h-screen flex flex-col">
-    <x-header.index />
-    <header class="px-4 sm:px-6 lg:px-8">
-        <x-title>{{ $title }}</x-title>
-    </header>
-    <main class="bg-gray-100 p-4 sm:px-6 lg:px-8 flex-1">
-        {{ $slot }}
-    </main>
-    <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
-</body>
-
-</html>
+    <body class="h-screen flex flex-col">
+        <x-header.index />
+        <header class="px-4 sm:px-6 lg:px-8">
+            <x-title>{{ $pageTitle }}</x-title>
+        </header>
+        <main class="bg-gray-100 p-4 sm:px-6 lg:px-8 flex-1">
+            {{ $slot }}
+        </main>
+        {{-- <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script> --}}
+    </body>
+</x-layouts.layout>
--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -1,37 +1,20 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="h-full">
+<x-layouts.layout>
 
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @livewireStyles
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="https://fonts.googleapis.com/css2?family=Inter:opsz,wght@14..32,100..900&display=swap"
-        rel="stylesheet" />
-    <title>{{ $title ?? 'Page Title' }}</title>
-</head>
+    <body class="min-h-screen flex flex-col" x-data="{ sidebarOpen: !window.matchMedia('(max-width: 639px)').matches }">
+        <x-dashboard.header />
+        <x-dashboard.sidebar />
+        <main class="bg-gray-100 p-4 flex-1 sm:px-6 lg:px-8" :class="{ 'sm:ml-64 ml-0': sidebarOpen }">
+            {{ $slot }}
+        </main>
 
-<body class="min-h-screen flex flex-col" x-data="{ sidebarOpen: !window.matchMedia('(max-width: 639px)').matches }">
-    <x-dashboard.header />
-    <x-dashboard.sidebar />
-    <main class="bg-gray-100 p-4 flex-1 sm:px-6 lg:px-8" :class="{ 'sm:ml-64 ml-0': sidebarOpen }">
-        {{ $slot }}
-    </main>
+        {{-- Toaster --}}
+        @if (session('status'))
+            <x-toaster theme="{{ session('status')['theme'] }}">{{ session('status')['message'] }}</x-toaster>
+        @endif
 
-    {{-- Toaster --}}
-    @if (session('status'))
-        <x-toaster theme="{{ session('status')['theme'] }}">{{ session('status')['message'] }}</x-toaster>
-    @endif
-
-    <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
-    @livewireScripts
-    @livewireChartsScripts
-    <script src="{{ asset('js/slugify.js') }}"></script>
-    {{-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> --}}
-</body>
-
-
-</html>
+        <script src="https://cdn.jsdelivr.net/npm/apexcharts"></script>
+        @livewireScripts
+        @livewireChartsScripts
+        <script src="{{ asset('js/slugify.js') }}"></script>
+    </body>
+</x-layouts.layout>
